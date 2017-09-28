e901f62b72d16d35fa15d21f63fdfc80fdf96049
watc2h <watc2h@gmail.com>
2017-09-28 15:48:13 +0000

    Finished 62

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[
+    'uses'=>'FrontEndController@index',
+    'as'=>'index'
+    ]);
+
 
 Auth::routes();
 
